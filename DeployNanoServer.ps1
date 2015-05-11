02d680e962e41d29cf9e08878c0e59f6f0383c5a
jangelfdez <me@joseangelfernandez.es>
2015-05-11 18:18:52 +0200

    First working version of the script

--- DeployNanoServer.ps1
+++ DeployNanoServer.ps1
@@ -1,13 +1,30 @@
-﻿[CmdletBinding()]
+﻿  <#
+    .SYNOPSIS 
+      Creates a Nano Server .vhd image based on the .ISO installation media previously downloaded.
+    .EXAMPLE
+      Prepares the Nano Server image with support for running inside a Virtual Machine
+      .\DeployNanoServer.ps1 -ComputerPackage -StoragePackage -FailoverClusterPackage -GuestPackage -IsoPath C:\ISO\file.iso
+
+      Prepares the Nano Server image with support for creating an Hyper-V node
+      .\DeployNanoServer.ps1 -ComputerPackage -StoragePackage -FailoverClusterPackage -GuestPackage -IsoPath C:\ISO\file.iso
+
+      Prepares the Nano Server image with support for creating a Scale-Out File Server in a cluster
+      .\DeployNanoServer.ps1 -ComputerPackage -StoragePackage -FailoverClusterPackage -GuestPackage -IsoPath C:\ISO\file.iso
+
+      
+  #>
+
+[CmdletBinding()]
 param (
   [switch]$ComputerPackage,
   [switch]$StoragePackage,
   [switch]$FailoverClusterPackage,
   [switch]$GuestPackage,
   [switch]$OemPackage,
-  [switch]$ReverseForwardersPackage
+  [switch]$ReverseForwardersPackage,
+  [Parameter(Mandatory=$true)]
+  [System.Uri] $IsoPath
 )
-
 
 # Environment for with DISM tools
 $DismFolder = "$env:TEMP\dism"
@@ -19,91 +36,103 @@ $MountedImageLetter = "D:"
 $NanoServerVhdName = "NanoServer-TechnicalPreview2.vhd"
 $NanoServerVhdPath = "$env:TEMP\$NanoServerVhdName"
 
-
 # Unnatend file XML Configuration Data
 $ComputerName = "NanoServer"
 $AdministratorPassword = "Passw0rd!"
 $OrganizationOwner = "Contoso"
 $OrganizationName = "Contoso SL"
 
+$Lang = "en-us"
+
 $UnattendXMLFileName = "Unattend.xml"
 
-# Files needed
-#$WindowsServer2016IsoName = "10074.0.150424-1350.fbl_impressive_SERVER_OEMRET_X64FRE_EN-US.ISO"
-
-$WindowsServer2016IsoName = "en_windows_server_technical_preview_2_x64_dvd_6651466.iso"
-$WindowsServer2016IsoUrl = "http://care.dlservice.microsoft.com/dl/download/6/D/D/6DDE7201-5753-454A-A7CF-94F909E8EC05/$WindowsServer2016ISO"
-
+# Files required
 $ConvertImageScriptUrl = "https://raw.githubusercontent.com/jangelfdez/DeployNanoServer/master/"
 $ConvertImageScriptName = "Convert-WindowsImage.ps1"
 
-# Download Windows Server 2016 Technical Preview 2 from TechNet Evaluation Center
-#Invoke-WebRequest -Uri $WindowsServer2016IsoUrl -OutFile $env:TEMP\Downloads\$WindowsServer2016IsoName
-
-# Download Convert-WindowsImage script from Script Gallery
+Write-Output "-> Downloading modified versión of Convert-WindowsImage script"
 Invoke-WebRequest -Uri $ConvertImageScriptUrl$ConvertImageScriptName -OutFile $env:TEMP\$ConvertImageScriptName | Out-Null
 
-# If you are using Windows 10 10074 it fails
-#Mount-DiskImage -ImagePath $env:TEMP\Downloads\$WindowsServer2016IsoName -StorageType ISO
+# Parsing ISO file path
+$splitUri = ($IsoPath.AbsolutePath.Split("/"))
+$WindowsServer2016IsoName = $splitUri[$splitUri.Count - 1]
 
-# Convert WMI image to a VHD
+# If you are using Windows 10 10074 there is a bug
+$os = Get-WmiObject -Class Win32_OperatingSystem
+$osVersion = [System.Version]::new($os.Version)
+
+if ( $osVersion.Major -ge 10 )
+{
+    Write-Output "`nThere is a problem with Mount-Disk Cmdlet on Windows 10. Please, mount the ISO file with a double click on the $WindowsServer2016IsoName ISO"
+    Read-Host -Prompt "Press any key to launch Explorer..."
+   
+    explorer $env:HOMEPATH
+    Read-Host -Prompt "Press Enter after you have mounted the image, doing it before the script will fail" 
+
+} else {
+    Write-Output "-> Mounting $WindowsServer2016IsoName .ISO image"
+    Mount-DiskImage -ImagePath $env:TEMP\$WindowsServer2016IsoName -StorageType ISO
+}
+ 
+
+Write-Output "-> Converting .wim file to .vhd"
 Invoke-Expression "$env:TEMP\$ConvertImageScriptName -Sourcepath '$MountedImageLetter\NanoServer\NanoServer.wim' -VHD $NanoServerVhdPath –VHDformat VHD -Edition 1"
 
-
-# Build a local directory to work with DISM
+Write-Output "`n-> Using $DismFolder folder"
 New-Item -Type Directory $DismFolder | Out-Null
 
-# Copying required files from the .ISO file
+Write-Output "-> Copying required files"
 Copy-Item -Path $MountedImageLetter\sources\api*downlevel*.dll -Destination $DismFolder
 Copy-Item -Path $MountedImageLetter\sources\*dism*.dll -Destination $DismFolder
 Copy-Item -Path $MountedImageLetter\sources\*provider*.dll -Destination $DismFolder
 
 Set-Location $DismFolder
 
-# Path were the image is going to be mounted
+Write-Output "-> Creating mount point"
 New-Item -Type Directory $CustomImageMountFolder | Out-Null
 
+Write-Output "-> Mounting image"
 Mount-WindowsImage -ImagePath "$env:TEMP\$NanoServerVhdName" -Path $CustomImageMountFolder -Index 1 | Out-Null
 
-# Adding support to Hyper-V
+Write-Output "-> Adding selected packages..."
 if ( $ComputerPackage ){
-
-    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\Microsoft-NanoServer-Compute-Package.cab" -Path $CustomImageMountFolder
-    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\en-us\Microsoft-NanoServer-Compute-Package.cab" -Path $CustomImageMountFolder
+    Write-Output "|-> Microsoft-NanoServer-Compute-Package.cab"
+    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\Microsoft-NanoServer-Compute-Package.cab" -Path $CustomImageMountFolder | Out-Null
+    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\$Lang\Microsoft-NanoServer-Compute-Package.cab" -Path $CustomImageMountFolder | Out-Null
 }
  
-# Adding support to Scale-Out File Server
-if ( $StoragePackage ) {
-    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\Microsoft-NanoServer-Storage-Package.cab" -Path $CustomImageMountFolder
-    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\en-us\Microsoft-NanoServer-Storage-Package.cab" -Path $CustomImageMountFolder
+if ( $StoragePackage ) { 
+    Write-Output "|-> Microsoft-NanoServer-Storage-Package.cab"
+    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\Microsoft-NanoServer-Storage-Package.cab" -Path $CustomImageMountFolder | Out-Null
+    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\$Lang\Microsoft-NanoServer-Storage-Package.cab" -Path $CustomImageMountFolder | Out-Null
 }
 
- # Adding support to Failover Clustering
 if ( $FailoverClusterPackage){
-    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\Microsoft-NanoServer-FailoverCluster-Package.cab" -Path $CustomImageMountFolder
-    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\en-us\Microsoft-NanoServer-FailoverCluster-Package.cab" -Path $CustomImageMountFolder
-}
+    Write-Output "|-> Microsoft-NanoServer-Storage-Package.cab"
+    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\Microsoft-NanoServer-FailoverCluster-Package.cab" -Path $CustomImageMountFolder | Out-Null
+    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\$Lang\Microsoft-NanoServer-FailoverCluster-Package.cab" -Path $CustomImageMountFolder | Out-Null
+} 
 
-# Adding support to run on a Virtual Machine
 if ( $GuestPackage ) {
-    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\Microsoft-NanoServer-Guest-Package.cab" -Path $CustomImageMountFolder
-    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\en-us\Microsoft-NanoServer-Guest-Package.cab" -Path $CustomImageMountFolder
+    Write-Output "|-> Microsoft-NanoServer-Guest-Package.cab"
+    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\Microsoft-NanoServer-Guest-Package.cab" -Path $CustomImageMountFolder | Out-Null
+    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\$Lang\Microsoft-NanoServer-Guest-Package.cab" -Path $CustomImageMountFolder | Out-Null
 }
 
-# Adding support to run on physical hardware
-if ( $OemPackage ){
-    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\Microsoft-NanoServer-OEM-Drivers-Package.cab" -Path $CustomImageMountFolder
-    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\en-us\Microsoft-NanoServer-OEM-Drivers-Package.cab" -Path $CustomImageMountFolder
+if ( $OemPackage) { 
+    Write-Output "|-> Microsoft-NanoServer-Storage-Package.cab"
+    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\Microsoft-NanoServer-OEM-Drivers-Package.cab" -Path $CustomImageMountFolder | Out-Null
+    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\$Lang\Microsoft-NanoServer-OEM-Drivers-Package.cab" -Path $CustomImageMountFolder | Out-Null
 }
  
-# Adding support to apps not developed for Nano Server
 if ( $ReverseForwardersPackage ){
-    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\Microsoft-OneCore-ReverseForwarders-Package.cab" -Path $CustomImageMountFolder
-    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\en-us\Microsoft-OneCore-ReverseForwarders-Package.cab" -Path $CustomImageMountFolder
+    Write-Output "|-> Microsoft-OneCore-ReverseForwarders-Package.cab"
+    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\Microsoft-OneCore-ReverseForwarders-Package.cab" -Path $CustomImageMountFolder | Out-Null
+    Add-WindowsPackage -PackagePath "$MountedImageLetter\NanoServer\Packages\$Lang\Microsoft-OneCore-ReverseForwarders-Package.cab" -Path $CustomImageMountFolder | Out-Null
 }
 
 
-# Apply the unattend installation file
+Write-Output "-> Applying unattend installation file"
 $UnattendXmlContent = "<?xml version='1.0' encoding='UTF-8'?> 
  <unattend xmlns='urn:schemas-microsoft-com:unattend' xmlns:wcm='http://schemas.microsoft.com/WMIConfig/2002/State' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'> 
  <settings pass='offlineServicing'> 
@@ -131,14 +160,13 @@ $UnattendXmlContent = "<?xml version='1.0' encoding='UTF-8'?>
  </unattend>"
 
 $UnattendXmlContent | Out-File "$DismFolder\$UnattendXMLFileName" -Encoding utf8
-
 Use-WindowsUnattend -UnattendPath "$DismFolder\$UnattendXMLFileName" -Path $CustomImageMountFolder | Out-Null
  
-# Copiamos el fichero en el directorio esperado dentro de Windows
+Write-Output "-> Copying unattend installation file inside the image"
 New-Item -Type Directory "$CustomImageMountFolder\Windows\panther" | Out-Null
 Copy-Item -Path "$DismFolder\$UnattendXMLFileName" -Destination "$CustomImageMountFolder\Windows\panther"
  
-# Copiamos el fichero en el directorio esperado dentro de Windows
+Write-Output "-> Copying SetupComplete.cmd file inside the image"
 New-Item -Type Directory $CustomImageMountFolder\Windows\Setup | Out-Null
 New-Item -Type Directory $CustomImageMountFolder\Windows\Setup\Scripts | Out-Null
 
@@ -146,9 +174,11 @@ $SetupCmdContent = "ipconfig"
 $SetupCmdContent | Out-File "$DismFolder\SetupComplete.cmd" -Encoding ascii
 
 Copy-Item -Path $DismFolder\SetupComplete.cmd -Destination $CustomImageMountFolder\Windows\Setup\Scripts
- 
+
+Write-Output "-> Dismounting image"
 Dismount-WindowsImage -Path $CustomImageMountFolder -Save | Out-Null
 
+Write-Output "-> Your Nano Server .vhd is available at $env:TEMP"
 
 # La IP será la que haya aparecido al arranque Nano Server.
 #Set-Item WSMan:\localhost\Client\TrustedHosts 192.168.1.39
